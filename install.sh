5f6616743f83be204a57db2c740e923a6c1f9884
Mohit Thakral <m@thakral.in>
2018-04-28 19:40:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/zshrc ~/.zshrc
+ln -s ~/.vim/tmux.conf ~/.tmux.conf
 cd ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugUpdate +qa
